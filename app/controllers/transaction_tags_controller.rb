274bd4a402b48ae741b6374f3594e68fe21ea2b7
Eric Heath <eric.heath@siriusxm.com>
2025-01-04 14:30:13 -0500

    Modal working ok

--- app/controllers/transaction_tags_controller.rb
+++ app/controllers/transaction_tags_controller.rb
@@ -7,20 +7,25 @@ class TransactionTagsController < ApplicationController
   end
 
   def create
-    tag_rule = nil
-    tag = Tag.find_or_create_by(user_id: current_user.id, tag: params[:transaction_tag][:tag])
-    if params[:search_string].present?
-      tag_rule = TagRule.find_or_create_by(tag_id: tag.id, search_string: params[:search_string])
+    @tag_rule = nil
+    @tag = Tag.find_or_create_by(user_id: current_user.id, tag: params[:transaction_tag][:tag])
+    if params[:search_string].present? && @tag.persisted?
+      @tag_rule = TagRule.find_or_create_by(tag_id: @tag.id, search_string: params[:search_string])
     end
-    if tag_rule.present?
-      tag_rule.apply
-    else
-      TransactionTag.create(user_transaction_id: params[:transaction_id], tag_id: tag.id)
+    if @tag_rule.present? && @tag_rule.persisted?
+      @tag_rule.apply
+    elsif @tag.persisted? && @tag.valid?
+      @transaction_tag = TransactionTag.create(user_transaction_id: params[:transaction_id], tag_id: @tag.id)
     end
     # create_notice
-    respond_to do |format|
-      format.html
-      format.turbo_stream
+    if @tag.persisted? && @tag.valid?
+      respond_to do |format|
+        format.html
+        format.turbo_stream
+      end
+    else
+      @user_transaction = current_user.user_transactions.find_by_id(params[:transaction_id])
+      render :new, status: :unprocessable_entity
     end
   end
 end
